3f8d03acc992a314692dd7826b456510c0fa31e8
Boris Brezillon <boris.brezillon@free-electrons.com>
2015-06-04 15:35:56 +0200

    mtd: add an MTD_IS_PARTITION flag
    Add an MTD_IS_PARTITION flag so that the test to check whether an
    MTD device is a partition or not is faster.
    This also allows user-space programs to know whether they are
    manipulating a partition or a real device.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>

--- drivers/mtd/mtdpart.c
+++ drivers/mtd/mtdpart.c
@@ -367,6 +367,7 @@ static struct mtd_part *allocate_partition(struct mtd_info *master,
 	/* set up the MTD object for this partition */
 	slave->mtd.type = master->type;
 	slave->mtd.flags = master->flags & ~part->mask_flags;
+	slave->mtd.flags |= MTD_IS_PARTITION;
 	slave->mtd.size = part->size;
 	slave->mtd.writesize = master->writesize;
 	slave->mtd.writebufsize = master->writebufsize;
